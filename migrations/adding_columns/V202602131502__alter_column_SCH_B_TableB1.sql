cbe077fe908bb1b07c964d285647a59f2125e8a7
Bhavana Vemula <bhavana.vemula@lpdg.io>
2026-02-13 15:39:41 +0530

    altered columns and added new schema

--- migrations/adding_columns/V202602131502__alter_column_SCH_B_TableB1.sql
+++ migrations/adding_columns/V202602131502__alter_column_SCH_B_TableB1.sql
@@ -1,5 +1,5 @@
 -- Add new columns to TableB1
-ALTER TABLE IF {{ database_name }}.SCHEMA_B.TableB1
+ALTER TABLE IF EXISTS {{ database_name }}.SCHEMA_B.TableB1
 ADD COLUMN discount NUMBER;
 
 ALTER  TABLE IF EXISTS {{ database_name }}.SCHEMA_B.TableB1
